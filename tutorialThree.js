fa45bdf78b537f086199811668ed63fd31d94443
IgweRoseline <120320491+IgweRoseline@users.noreply.github.com>
2023-08-12 20:24:48 +0100

    tutorialThree.js

---
+++ tutorialThree.js
@@ -0,0 +1,13 @@
+let y = 1;
+let x = 2;
+let z = 3;
+let a = y/x;
+let b = 3 * a; 
+let c = 2 * a; 
+let d = b + c; 
+let e = z - 1;
+let f = 5**3;
+let g = f * e; 
+let h = g * z; 
+let m = d/h;
+console.log(m);
